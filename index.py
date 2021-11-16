1cf0c6c8660a457a87a7f3fd01efab358067ed9c
Zoxir Rustamov <zoxabtw77@gmail.com>
2021-11-16 20:00:14 +0500

    lesson 6

---
+++ index.py
@@ -0,0 +1,30 @@
+# age = int(input("enter your age"))
+
+# if age == 18 and age <= 50:
+#     print('Welcome')
+# else: print('bye')
+
+# num = int(input("enter number"))
+# print(num % 10)
+
+# cost =int(input('Введите сумму заказа'))
+# money =int(input('Введите сумму которую вы дали'))1
+
+
+# if cost % 100  == 0 and money % 500 == 0:
+#     change = money - cost 
+#     five_hunderds = change // 500
+#     change = change % 500
+    
+#     two_hunderds = change // 200
+#     change = change % 200
+
+#     five_hunderds = change // 100
+#     print('five_hinderds, two_hinderds,hinderds ')
+
+a=float(input('enter number'))
+b=float(input('enter number'))
+c=float(input('enter number'))
+
+if a > b > c: print(a, b, c)
+print(a, b, c)
